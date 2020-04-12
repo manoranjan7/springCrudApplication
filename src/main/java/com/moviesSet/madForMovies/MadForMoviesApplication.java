43f672e8aaa05b95e6115cf835ec6ee5daaca121
Manoranjan man <manoranjans375@gmail.com>
2020-04-13 01:19:37 +0530

    Created a Base project

---
+++ src/main/java/com/moviesSet/madForMovies/MadForMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.moviesSet.madForMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadForMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadForMoviesApplication.class, args);
+	}
+
+}
